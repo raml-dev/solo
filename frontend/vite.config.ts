1f9ed5eed4ff920fbeb818de02dbbf11a6cc779a
Riccardo <r.cannella.90@gmail.com>
2026-03-14 22:22:22 +0100

    chore(ide): add settings for ts config in vscode

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
-import path from "path";
+import { resolve } from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
   resolve: {
     alias: {
-      $lib: path.resolve(__dirname, "src/lib")
+      $wails: resolve(__dirname, "wailsjs"),
+      $src: resolve(__dirname, "src")
     }
   }
 });
